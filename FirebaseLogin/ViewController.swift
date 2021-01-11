bb3c7ccacdb37905021c675a236651fbf38027e5
Ignacio Mariani <ignacio.mariani@reactivereality.com>
2021-01-11 10:06:54 -0300

    Initial Commit

---
+++ FirebaseLogin/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirebaseLogin
+//
+//  Created by Ignacio on 11/01/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
